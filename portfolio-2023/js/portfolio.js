9fcb0e2723948f1d4407e73b9d5c5b73d220fd8b
park yu-mi <133612449+yumdidi@users.noreply.github.com>
2023-06-13 16:57:25 +0900

    Add files via upload

---
+++ portfolio-2023/js/portfolio.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.like-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.art').slick({
+    // autoplay: true,
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true,
+    });
+});
